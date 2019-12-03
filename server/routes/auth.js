12103f75f47e1da9f96daf8ac98735a8da24bb57
Firdaus Nusa <firdausnusa.parity@gmail.com>
2019-12-03 13:04:50 +0800

    repaired bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const {registerValidation, loginValidation, updateValidation } = require('./validation')
 

d55ad752f12884968c2f64c71e25fafdf2139e94
Firdaus Nusa <firdausnusa.parity@gmail.com>
2019-12-03 14:31:37 +0800

    repair mysql connection/end connection

--- server/subjects/subjects.js
+++ server/subjects/subjects.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 const mysql = require('mysql')
 
 //connect mysql db
-const config = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'b1fadc3c9ec71c',
     password: 'c4325777',
     database: 'heroku_dd0c314e35a5b93'
 });
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected to mysql database')
+})
+    
 
 router.get('/', (req, res) => {
-    const connection = mysql.createConnection(config)
     connection.query('SELECT * FROM subjects', (err, result, field) => {
         connection.end()
         if (!err) {
@@ -22,7 +26,6 @@ router.get('/', (req, res) => {
 })
 
 router.get('/category', (req, res) => {
-    const connection = mysql.createConnection(config)
     connection.query('SELECT category FROM subjects', (err, result, field) => {
         connection.end()
         if (!err) {
